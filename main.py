be7985e9cc0588aad09e8e3b1fe8be4d57ae33f8
Shun702 <138392252+Shun702@users.noreply.github.com>
2024-08-14 03:20:35 +0100

    library import and data collection

---
+++ main.py
@@ -0,0 +1,26 @@
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['text.color'] = 'k'
+
+df = pd.read_excel("Superstore.xls")
+furniture = df.loc[df['Category'] == 'Furniture']
+
+furniture['Order Date'].min(), furniture['Order Date'].max()
+
+#remove unwanted coolumns, check missing values, aggregate sales by date 
+cols = ['Row ID', 'Order ID', 'Ship Date', 'Ship Mode', 'Customer ID', 'Customer Name', 'Segment', 'Country', 'City', 'State', 'Postal Name', 'Quantity', 'Discount', 'Profit']
+furniture.drop(cols, axis=1, inplacee=True)
+furniture = furniture.sort_values('Order Date')
+
+furniture.isnull().sum()
